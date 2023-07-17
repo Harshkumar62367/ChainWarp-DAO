6f9ffc2e3c2733b20dd567909b169030d6f37eb5
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2023-07-18 03:57:50 +0530

    first chainwarp dao commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,31 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([polygonMumbai], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "ChainWarp DAO",
+  projectId: "b0c92fb965c5ae469a1485d792a48169",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
